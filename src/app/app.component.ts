d8fcb60a8705dadb81db2b0f67ae5c939237a1a2
NirmalGit <NirmalGit@users.noreply.github.com>
2023-08-02 12:54:58 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -85,19 +85,20 @@ export class AppComponent {
         stacking: 'normal',
       },
     },
-    series: [
-      {
-        name: 'John',
-        data: [5, 3, 4, 7, 2],
-      },
-      {
-        name: 'Jane',
-        data: [2, 2, 3, 2, 1],
-      },
-      {
-        name: 'Joe',
-        data: [3, 4, 4, 2, 5],
-      },
-    ],
+    series: this.projectData
+    // series: [
+    //   {
+    //     name: 'John',
+    //     data: [5, 3, 4, 7, 2],
+    //   },
+    //   {
+    //     name: 'Jane',
+    //     data: [2, 2, 3, 2, 1],
+    //   },
+    //   {
+    //     name: 'Joe',
+    //     data: [3, 4, 4, 2, 5],
+    //   },
+    // ],
   };
 }
